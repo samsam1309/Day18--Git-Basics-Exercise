df2c6ad4e0c70c9a2da28b638af46631a3cb49ee
John DoeSamuel <samuelsebban008@gmail.com>
2024-02-13 17:36:39 +0200

    Update the onClick function

--- js/main.js
+++ js/main.js
@@ -2,5 +2,13 @@
 
 
 function onBallClick(elBtn) {
-    console.log("HELLO!")
+    var ball = document.querySelector(".ball");
+    var currentHeight = parseInt(ball.style.height, 10) || 100; // Parse or default to 100
+    var currentWidth = parseInt(ball.style.width, 10) || 100;   // Parse or default to 100
+
+    var newHeight = currentHeight + 50;
+    var newWidth = currentWidth + 50;
+
+    ball.style.height = newHeight + "px";
+    ball.style.width = newWidth + "px";
 }
