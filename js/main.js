c047a5f91f4d2ce01795d4d4287ab4ebfa2e6d6d
John DoeSamuel <samuelsebban008@gmail.com>
2024-02-13 17:41:09 +0200

    Add more styling rules

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 
 function onBallClick(elBtn) {
     var ball = document.querySelector(".ball");
-    var currentHeight = parseInt(ball.style.height, 10) || 100; // Parse or default to 100
-    var currentWidth = parseInt(ball.style.width, 10) || 100;   // Parse or default to 100
+    var currentHeight = parseInt(ball.style.height, 10) || 100; 
+    var currentWidth = parseInt(ball.style.width, 10) || 100;   
 
     var newHeight = currentHeight + 50;
     var newWidth = currentWidth + 50;
